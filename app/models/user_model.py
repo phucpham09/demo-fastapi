f222f2fbf53507cadae9a70eacab6172563a6dd0
phucpham09 <phamhuuphuc4049@gmail.com>
2025-05-02 20:14:56 +0700

    feat(api): excluding the password field in returned result

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -3,10 +3,10 @@ from sqlmodel import SQLModel, Field
 class UserBase(SQLModel):
     username: str = Field(index=True)
     email: str
-    password: str
 
 class User(UserBase, table=True):
     id: int | None = Field(primary_key=True, default=None)
+    password: str
     
 class UserCreate(UserBase):
     password: str
